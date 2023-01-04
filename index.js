9e3484c11cad90690bcbd2dcffab1ac4c8df99c2
Bikrom-Roy <bikromroy2001@gmail.com>
2023-01-04 23:50:36 +0600

    server site

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const mg = require('nodemailer-mailgun-transport');
 require('dotenv').config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -16,6 +21,54 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wwiopku.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function sendBookingEmail(booking) {
+    const { email, treatment, appointmentDate, slot } = booking;
+
+    // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+    const auth = {
+        auth: {
+            api_key: process.env.EMAIL_SEND_KEY,
+            domain: process.env.EMAIL_SEND_DOMAIN
+        }
+    }
+
+    const transporter = nodemailer.createTransport(mg(auth));
+
+
+
+    // let transporter = nodemailer.createTransport({
+    //     host: 'smtp.sendgrid.net',
+    //     port: 587,
+    //     auth: {
+    //         user: "apikey",
+    //         pass: process.env.SENDGRID_API_KEY
+    //     }
+    // })
+
+    transporter.sendMail({
+        from: "bikromroy2001@gmail.com", // verified sender email
+        to: email, // recipient email
+        subject: `Your appointment for ${treatment} is confirmed`, // Subject line
+        text: "Hello world!", // plain text body
+        html: `
+        <h3>Your appointment is confirmed </h3>
+        <div>
+            <p> Your appointment for treatment ${treatment}</p>
+            <p> Please visite us on ${appointmentDate} at ${slot}</p>
+            <p> Thanks from doctors portals </p>
+        </div>
+        `, // html body
+    }, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
 
 
 function verifyJWT(req, res, next) {
@@ -43,10 +96,11 @@ async function run() {
         const bookingCollection = client.db('doctorsPortal').collection('bookings');
         const usersCollection = client.db('doctorsPortal').collection('users');
         const doctorsCollection = client.db('doctorsPortal').collection('doctors');
+        const paymentsCollection = client.db('doctorsPortal').collection('payments');
 
 
-         // NOTE: make sure you use verifyAdmin after verifyJWT
-         const verifyAdmin = async (req, res, next) =>{
+        // NOTE: make sure you use verifyAdmin after verifyJWT
+        const verifyAdmin = async (req, res, next) => {
             const decodedEmail = req.decoded.email;
             const query = { email: decodedEmail };
             const user = await usersCollection.findOne(query);
@@ -76,7 +130,7 @@ async function run() {
             res.send(options);
         });
 
-        app.get('/bookings', verifyJWT, async(req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded.email;
 
@@ -84,9 +138,16 @@ async function run() {
                 return res.status(403).send({ message: 'forbidden access' });
             }
 
-            const query = {email: email};
+            const query = { email: email };
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
+        });
+
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
         })
 
 
@@ -100,12 +161,14 @@ async function run() {
             }
             const alreadyBooked = await bookingCollection.find(query).toArray();
 
-            if(alreadyBooked.length){
+            if (alreadyBooked.length) {
                 const message = `You already have a booking on ${booking.appointmentDate}`;
-                return res.send({acknowledged: false, message});
+                return res.send({ acknowledged: false, message });
             }
 
             const result = await bookingCollection.insertOne(booking);
+            // seen email address about appointment confirmation 
+            sendBookingEmail(booking);
             res.send(result);
         });
 
@@ -132,6 +195,7 @@ async function run() {
                 {
                     $project: {
                         name: 1,
+                        price: 1,
                         slots: 1,
                         booked: {
                             $map: {
@@ -145,6 +209,7 @@ async function run() {
                 {
                     $project: {
                         name: 1,
+                        price: 1,
                         slots: {
                             $setDifference: ['$slots', '$booked']
                         }
@@ -155,50 +220,84 @@ async function run() {
             res.send(options);
         });
 
-        app.get('/appointmentSpecialty', async(req, res) => {
+        app.get('/appointmentSpecialty', async (req, res) => {
             const query = {};
-            const result = await appointmentOptionCollection.find(query).project({name: 1}).toArray();
+            const result = await appointmentOptionCollection.find(query).project({ name: 1 }).toArray();
+            res.send(result);
+        });
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+
+            const updateResult = await bookingCollection.updateOne(filter, updatedDoc);
+
             res.send(result);
         })
 
-        app.get('/jwt', async(req, res) => {
+        app.get('/jwt', async (req, res) => {
             const email = req.query.email;
-            const query = {email: email};
+            const query = { email: email };
             const user = await usersCollection.findOne(query);
-            if(user){
-                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '10d'});
-                return res.send({accessToken: token});
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '10d' });
+                return res.send({ accessToken: token });
             }
-            res.status(403).send({accessToken: ''});
+            res.status(403).send({ accessToken: '' });
         });
 
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
         });
 
-        app.get('/users/admin/:email', async(req, res) => {
+        app.get('/users/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {email}
+            const query = { email }
             const user = await usersCollection.findOne(query);
-            res.send({isAdmin: user?.role === 'admin'});
+            res.send({ isAdmin: user?.role === 'admin' });
         });
 
 
-        app.post('/users', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
 
 
-        app.put('/users/admin/:id',verifyJWT, verifyAdmin,  async(req, res) => {
-           
+        app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
+
 
             const id = req.params.id;
-            const filter = {_id: ObjectId(id)};
-            const options = {upsert: true};
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
             const updateDoc = {
                 $set: {
                     role: 'admin'
@@ -208,24 +307,39 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/doctors', verifyJWT, verifyAdmin, async(req, res) => {
+        app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const query = {};
             const doctors = await doctorsCollection.find(query).toArray();
             res.send(doctors);
         })
 
-        app.post('/doctors', verifyJWT,verifyAdmin, async(req, res) => {
+        app.post('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const doctor = req.body;
             const result = await doctorsCollection.insertOne(doctor);
             res.send(result);
+
         })
 
-        app.delete('/doctors/:id', verifyJWT, verifyAdmin, async(req, res) => {
+        app.delete('/doctors/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const result = await doctorsCollection.deleteOne(filter);
             res.send(result);
         })
+
+
+        // temporary to update price field on appointment options
+        // app.get('/addPrice', async (req, res) => {
+        //     const filter = {}
+        //     const options = { upsert: true }
+        //     const updatedDoc = {
+        //         $set: {
+        //             price: 99
+        //         }
+        //     }
+        //     const result = await appointmentOptionCollection.updateMany(filter, updatedDoc, options);
+        //     res.send(result);
+        // })
 
 
 
